43787d6bdb08f07b150babdbfa79c273247d7390
Rafael Silva <invites@rafaelsilva.net>
2014-04-08 23:51:01 -0300

    Modificações básicas
    Adicionada a View de adição bem como seu controller e uma barra

---
+++ iBeauty/GTAddViewController.h
@@ -0,0 +1,13 @@
+//
+//  GTAddViewController.h
+//  iBeauty
+//
+//  Created by Rafael Ferreira Silva on 08/04/14.
+//  Copyright (c) 2014 GroupOfTwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GTAddViewController : UIViewController
+
+@end
--- iBeauty/GTMasterViewController.m
+++ iBeauty/GTMasterViewController.m
@@ -25,11 +25,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-  self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-  UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-  self.navigationItem.rightBarButtonItem = addButton;
 }
 
 - (void)didReceiveMemoryWarning
